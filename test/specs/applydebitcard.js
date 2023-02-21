5d83121274054165c8e23325456ddc63c1a8b4da
Kavyar2804 <kavyar2804@gmail.com>
2023-02-21 12:27:33 +0530

    updates

--- test/specs/applydebitcard.js
+++ test/specs/applydebitcard.js
@@ -7,4 +7,26 @@ it('Debitcard Application' , async ()=>{
     await browser.scroll(0, 500)
     let ele = await browser.$("[li=Apply Debit Card]")
     await ele.waitForClickable({timeout:3000 })
-    await ele.click()
+    await ele.click()
+   let  title = await browser.getTitle()
+   console.log(title);
+    await expect (browser).toHaveTitleContaining("Apply Debit Card")
+    await browser.$("[name='holder_name']").setValue('Kavya')
+    await browser.$("[name='pan']").setValue('abcd1234k')
+    await browser.$("[name='mob']").setValue('1234567890')
+
+
+    
+    await browser.$("[name='acc_no']").setValue("")
+
+
+
+
+
+
+
+
+})
+
+
+})
