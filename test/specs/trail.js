571bb3ab5e0900baf14220533d83b9a335878846
Kavyar2804 <kavyar2804@gmail.com>
2023-07-11 13:41:05 +0530

    jenkins1235

--- test/specs/trail.js
+++ test/specs/trail.js
@@ -2,7 +2,7 @@ describe('Launch chrome browser', ()=>{
 
     it('launch browser' , async()=>{
 
-        await browser.url('http://rmgtestingserver/domain/Online_Banking_System')
+        await browser.url('http://www.google.com')
         await browser.pause(5000)
     })
 })
